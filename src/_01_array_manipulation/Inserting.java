37825f8ebac7071909810802fed987799bd7e9ae
EricrPark <ericpark2004@gmail.com>
2018-07-17 15:02:03 -0700

    old fashioned arrays

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -3,5 +3,30 @@ package _01_array_manipulation;
 
 
 public class Inserting {
+
+	public static int[] insertAt(int[] testArray, int i, int j) {
+		// TODO Auto-generated method stub
+		int[] testArray1 = new int[testArray.length + 1];
+		for (int k = 0; k < testArray.length; k++) {
+			if (k < i) {
+				testArray1[k] = testArray[k];
+			} else if (k >= i) {
+				testArray1[k + 1] = testArray[k];
+			}
+		}
+		testArray1[i] = j;
+		return testArray1;
+	}
+
+	public static String[] insertAlphabetically(String[] orderedArray, String string) {
+		// TODO Auto-generated method stub
+		String[] orderedArray1 = new String[orderedArray.length + 1];
+		for (int i = 0; i < orderedArray.length; i++) {
+			orderedArray1[i] = orderedArray[i];
+		}
+		orderedArray1[orderedArray.length] = string;
+		Sorting.sort(orderedArray1);
+		return orderedArray1;
+	}
 	
 }
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,16 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static void sort(String[] testArray) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < testArray.length; i++) {
+			for (int j = 0; j < testArray.length - 1; j++) {
+				if (testArray[j].compareTo(testArray[j + 1]) > 0) {
+					String temp = testArray[j];
+					testArray[j] = testArray[j + 1];
+					testArray[j + 1] = temp;
+				}
+			}
+		}
+	}
 }
